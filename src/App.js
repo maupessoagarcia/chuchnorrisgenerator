999d3510d9aea430cc05eeaa7201833f48e92194
Mauricio Garcia <maupessoagarcia@yahoo.com>
2023-05-02 05:17:21 +0200

    Added image

--- src/App.js
+++ src/App.js
@@ -19,7 +19,15 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Chuck Norris Random Joke Generator</h1>
+      <img
+        src="https://blackbeltmag.com/media-library/chuck-norris-movies.jpg?id=25805225&width=620&height=350"
+        alt="Chuck Norris"
+      />
+      <br />
+      <br />
       <Joke joke={joke} />
+      <br />
+      <br />
       <RefreshButton onClick={fetchJoke} />
     </div>
   );
